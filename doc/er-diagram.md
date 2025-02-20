834c3ccc2fc83ae5d981aab2b65e44f2062f98ed
Jacob Lévesque <jaclev34@gmail.com>
2025-02-20 10:55:58 -0500

    Ajout de la table service

--- doc/er-diagram.md
+++ doc/er-diagram.md
@@ -23,7 +23,7 @@ erDiagram
     }
     tables {
         uuid table_id PK
-        uuid restaurant_id FK
+        uuid table_restaurant_id FK
         int number
         int seats
         bool is_deleted
@@ -33,14 +33,24 @@ erDiagram
     }
     reservations {
         uuid reservation_id PK
-        uuid user_id FK
-        uuid table_id FK
+        uuid reservation_user_id FK
+        uuid reservation_table_id FK
+        uuid reservation_service_id FK
         int seats_taken
-        timestamptz start_time
-        timestamptz end_time
         timestamptz created_at
         timestamptz updated_at
     }
+
+    services {
+        uuid service_id PK
+        timestamptz start_time
+        timestamptz end_time
+        bool repeting
+        int repeting_days_bitmask
+        timestamptz created_at
+        timestamptz updated_at
+    }
+
     users ||--o{ reservations : ""
     restaurants ||--o{ tables : ""
     tables ||--o{ reservations : ""
--- src/db/init.sql
+++ src/db/init.sql
@@ -25,53 +25,68 @@ CREATE TABLE restaurants (
 
 CREATE TABLE tables (
     table_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    restaurant_id UUID NOT NULL REFERENCES restaurants(restaurant_id) ON DELETE CASCADE,
+    table_restaurant_id UUID NOT NULL REFERENCES restaurants(restaurant_id) ON DELETE CASCADE,
     number INTEGER NOT NULL CHECK (number > 0),
     seats INTEGER NOT NULL CHECK (seats > 0),
     is_deleted BOOLEAN DEFAULT FALSE,
     deleted_at TIMESTAMPTZ,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(restaurant_id, number)
+    UNIQUE(table_restaurant_id, number)
+);
+
+CREATE TABLE services (
+    service_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ NOT NULL CHECK (end_time > start_time),
+    is_repeting BOOLEAN DEFAULT FALSE,
+    repeating_days_bitmask INT NOT NULL DEFAULT 0 CHECK (repeating_days_bitmask BETWEEN 0 AND 127),
+    is_deleted BOOLEAN DEFAULT FALSE,
+    deleted_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT valid_reservation_duration CHECK (end_time - start_time <= INTERVAL '4 hours')
 );
 
 CREATE TABLE reservations (
     reservation_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-    table_id UUID NOT NULL REFERENCES tables(table_id) ON DELETE CASCADE,
+    reservation_user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    reservation_table_id UUID NOT NULL REFERENCES tables(table_id) ON DELETE CASCADE,
+    reservation_service_id UUID NOT NULL REFERENCES services(service_id) ON DELETE CASCADE,
     seats_taken INTEGER NOT NULL CHECK (seats_taken > 0),
-    start_time TIMESTAMPTZ NOT NULL,
-    end_time TIMESTAMPTZ NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT valid_time_range CHECK (end_time > start_time),
-    CONSTRAINT valid_reservation_duration CHECK (end_time - start_time <= INTERVAL '4 hours')
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-  NEW.updated_at = CURRENT_TIMESTAMP;
-  RETURN NEW;
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_users
     BEFORE UPDATE ON users
     FOR EACH ROW
     EXECUTE FUNCTION trigger_set_timestamp();
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_restaurants
     BEFORE UPDATE ON restaurants
     FOR EACH ROW
     EXECUTE FUNCTION trigger_set_timestamp();
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_tables
     BEFORE UPDATE ON tables
     FOR EACH ROW
     EXECUTE FUNCTION trigger_set_timestamp();
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_services
+    BEFORE UPDATE ON services
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp_reservations
     BEFORE UPDATE ON reservations
     FOR EACH ROW
     EXECUTE FUNCTION trigger_set_timestamp();
@@ -85,14 +100,20 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER soft_delete_user
+CREATE TRIGGER soft_delete_users
     BEFORE UPDATE OF is_deleted ON users
     FOR EACH ROW
     WHEN (NEW.is_deleted = TRUE AND OLD.is_deleted = FALSE)
     EXECUTE FUNCTION soft_delete();
 
-CREATE TRIGGER soft_delete_table
+CREATE TRIGGER soft_delete_tables
     BEFORE UPDATE OF is_deleted ON tables
+    FOR EACH ROW
+    WHEN (NEW.is_deleted = TRUE AND OLD.is_deleted = FALSE)
+    EXECUTE FUNCTION soft_delete();
+
+CREATE TRIGGER soft_delete_services
+    BEFORE UPDATE OF is_deleted ON services
     FOR EACH ROW
     WHEN (NEW.is_deleted = TRUE AND OLD.is_deleted = FALSE)
     EXECUTE FUNCTION soft_delete();
